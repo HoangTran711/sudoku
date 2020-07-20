87715c1085c806140653cbf64531b669717e8a3f
My Name <tranhoang.finizz@gmail.com>
2020-07-20 18:03:51 +0700

    build

---
+++ build/precache-manifest.b5cc7364be1f7ee0555d13f12dca6908.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb14d264d1d4708b93a002bd42dc2295",
+    "url": "./index.html"
+  },
+  {
+    "revision": "17046ccf379ac3e2b29c",
+    "url": "./static/css/2.fb8d7c45.chunk.css"
+  },
+  {
+    "revision": "8afbd655d5a3ebeea495",
+    "url": "./static/css/main.6469227c.chunk.css"
+  },
+  {
+    "revision": "17046ccf379ac3e2b29c",
+    "url": "./static/js/2.93ddafc2.chunk.js"
+  },
+  {
+    "revision": "32d0ce4f3941df3ad4661a6e1edd2498",
+    "url": "./static/js/2.93ddafc2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8afbd655d5a3ebeea495",
+    "url": "./static/js/main.8d76d9fc.chunk.js"
+  },
+  {
+    "revision": "f188e86e594ce377a443",
+    "url": "./static/js/runtime-main.366a0dd7.js"
+  }
+]);
